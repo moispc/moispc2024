0246e6e4644a2c8e2a49da7c081449fc0b3b6919
Agustin G <141699827+agustinnicolasgallardorios@users.noreply.github.com>
2024-05-09 09:18:56 -0300

    Solucion a componentes "Standalone"

--- frontend 2024/app/src/app/app.component.ts
+++ frontend 2024/app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { CheckoutComponent } from './components/shared/checkout/checkout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DashboardComponent, CheckoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
