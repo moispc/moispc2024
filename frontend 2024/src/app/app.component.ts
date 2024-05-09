8c56cb2412616d6c4f9add83137aab4853cfbc84
Cristian López <cristianlp.ok@gmail.com>
2024-05-09 20:32:14 -0300

    Update app.component.ts

--- frontend 2024/src/app/app.component.ts
+++ frontend 2024/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CarritoComponent } from './pages/carrito/carrito.component';
+import { ExitoComponent } from './pages/exito/exito.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CarritoComponent],
+  imports: [RouterOutlet, CarritoComponent, ExitoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
