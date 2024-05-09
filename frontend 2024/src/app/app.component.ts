55c00f9cde266316d16818baebd5c41160a3d0bb
Veronica Ludemann <veronicaludemann67@gmail.com>
2024-05-09 12:23:52 -0300

    se acomoda la estructura de carpetas ya que se habia nombrado mal una de ellas

--- frontend 2024/src/app/app.component.ts
+++ frontend 2024/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CarritoComponent } from './components/pages/carrito/carrito.component';
+import { CarritoComponent } from './pages/carrito/carrito.component';
 
 @Component({
   selector: 'app-root',
