cdbc97875b466f58320078888e3d60980f071f21
Veronica Ludemann <veronicaludemann67@gmail.com>
2024-05-10 14:01:23 -0300

    se soluciona problemas de importaciones

--- frontend 2024/src/app/app.component.ts
+++ frontend 2024/src/app/app.component.ts
@@ -4,11 +4,18 @@ import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CheckoutComponent } from './shared/checkout/checkout.component';
 import { CarritoComponent } from './pages/carrito/carrito.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NosotrosDevsComponent } from './pages/nosotros-devs/nosotros-devs.component';
+
+import { HeaderComponent } from './shared/header/header.component';
+import { NavComponent } from './shared/nav/nav.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DashboardComponent, CheckoutComponent, CarritoComponent],
+  imports: [ CommonModule, RouterOutlet, CarritoComponent, HomeComponent,NosotrosDevsComponent, HeaderComponent, NavComponent,DashboardComponent,CheckoutComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend 2024/src/app/app.routes.ts
+++ frontend 2024/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CarritoComponent } from './pages/carrito/carrito.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"home", component:HomeComponent},
+    {path:"carrito", component:CarritoComponent}
+
+];
