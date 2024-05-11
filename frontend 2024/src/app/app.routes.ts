8a4001300f3fa2d526177b3d8d796e711cc402fc
Veronica Ludemann <veronicaludemann67@gmail.com>
2024-05-11 11:03:38 -0300

    cambios en las rutas

--- frontend 2024/src/app/app.routes.ts
+++ frontend 2024/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { CartaComponent } from './pages/carta/carta.component';
 
 export const routes: Routes = [
     {path:"home", component:HomeComponent},
+    {path:"", redirectTo:"/home", pathMatch:"full"},
     {path:"carta", component:CartaComponent},
     {path:"carrito", component:CarritoComponent},
 
