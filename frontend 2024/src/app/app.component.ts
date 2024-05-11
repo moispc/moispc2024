198747578f3b716341683fee4cdd9baea3f4cc88
Gisele Lavisse <gigilavisse@gmail.com>
2024-05-11 11:11:23 -0300

    se realizo footer

--- frontend 2024/src/app/app.component.ts
+++ frontend 2024/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CheckoutComponent } from './shared/checkout/checkout.component';
 import { CarritoComponent } from './pages/carrito/carrito.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DashboardComponent, CheckoutComponent, CarritoComponent],
+  imports: [CommonModule, RouterOutlet, DashboardComponent, CheckoutComponent, CarritoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
