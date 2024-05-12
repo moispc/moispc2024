b75c11355a99be6d0f33de501cff1c865206699a
Agustin G <141699827+agustinnicolasgallardorios@users.noreply.github.com>
2024-05-12 12:18:04 -0300

    Agregar Dashboard y Checkout al App Routing

--- frontend 2024/src/app/app.routes.ts
+++ frontend 2024/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CarritoComponent } from './pages/carrito/carrito.component';
 import { CartaComponent } from './pages/carta/carta.component';
+import { CheckoutComponent } from './shared/checkout/checkout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path:"home", component:HomeComponent},
     {path:"", redirectTo:"/home", pathMatch:"full"},
     {path:"carta", component:CartaComponent},
     {path:"carrito", component:CarritoComponent},
-
-
+    {path:"pagar", component:CheckoutComponent},
+    {path:"dashboard", component:DashboardComponent}
 ];
