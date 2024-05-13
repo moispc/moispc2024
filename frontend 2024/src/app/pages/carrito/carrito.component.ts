a741a180569fbe8c7b4c53075cdb34c59b25c61d
lucasGelpi <lucasglp32@gmail.com>
2024-05-12 22:09:21 -0300

    Mejoras estéticas del carrito y algo de funcionalidad

--- frontend 2024/src/app/pages/carrito/carrito.component.ts
+++ frontend 2024/src/app/pages/carrito/carrito.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-carrito',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './carrito.component.html',
   styleUrl: './carrito.component.css'
 })
 export class CarritoComponent {
-
+  productos = [
+    { 
+      nombre: 'Empanadas Árabes', 
+      cantidad: 6, 
+      precio: 10.99, 
+      imagenUrl: './assets/carta/arabes_thumbnail.webp' 
+    },
+    { 
+      nombre: 'Empanadas de J&Q', 
+      cantidad: 4, 
+      precio: 15.99, 
+      imagenUrl: './assets/carta/j&q_thumbnail.webp' 
+    }
+  ];
 }
