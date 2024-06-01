7d396a3d704160403143863e15bea9d1f0005b1f
Cristian López <cristianlp.ok@gmail.com>
2024-06-01 17:45:58 -0300

    update Component Exito

--- frontend 2024/src/app/pages/exito/exito.component.ts
+++ frontend 2024/src/app/pages/exito/exito.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-exito',
@@ -7,12 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './exito.component.html',
   styleUrl: './exito.component.css'
 })
-export class ExitoComponent {
-
+export class ExitoComponent implements OnInit {
+  contador=30;
+constructor(private router:Router){}
  printDiv() {
     window.print();
     
     window.location.href = './home';
 }
+ngOnInit(): void {
+  this.startCountdown();
+}
 
+startCountdown(){
+  const contdownInterval=setInterval(() => {this.contador--;
+    if(this.contador==0){
+      clearInterval(contdownInterval);
+      this.router.navigate(['/']);
+    }
+  },1000);
+    
+}
 }
