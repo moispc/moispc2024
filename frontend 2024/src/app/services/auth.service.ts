85f02640c7d40d32ecaec0e382e8cea44b37b887
Veronica Ludemann <veronicaludemann67@gmail.com>
2024-06-02 17:56:00 -0300

    se suben cambios de inicio-sesion

--- frontend 2024/src/app/services/auth.service.ts
+++ frontend 2024/src/app/services/auth.service.ts
@@ -3,22 +3,25 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private apiUrl = 'http://localhost:3000'; // Reemplazar con la URL de LA API
+  private apiUrl = 'http://localhost:8000/appUSERS/';
 
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string): Observable<boolean> {
     // Simula una llamada HTTP POST a tu API de autenticación
-    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, { email, password }).pipe(
+    return this.http.post<{ token: string, user_id: string }>(`${this.apiUrl}login/`, { email, password }).pipe(
       map(response => {
         // Guardar el token en localStorage o en algún lugar seguro
         localStorage.setItem('authToken', response.token);
+        localStorage.setItem ('idUser', response.user_id )
         return true;
+
       }),
       catchError(error => {
         console.error('Error en la autenticación', error);
@@ -30,6 +33,7 @@ export class AuthService {
   logout() {
     // Eliminar el token de autenticación
     localStorage.removeItem('authToken');
+    localStorage.removeItem('idUser');
   }
 
   isAuthenticated(): boolean {
