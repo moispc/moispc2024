9c23e6aa1130276ba19b137deb602443a094783e
Cristian López <cristianlp.ok@gmail.com>
2024-06-01 21:52:04 -0300

    Update Componente Pedidos

--- frontend 2024/src/app/services/pedidos.service.ts
+++ frontend 2024/src/app/services/pedidos.service.ts
@@ -1,27 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { DetallePedido, Pedido } from '../model/pedido.model';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class PedidosService {
 
-  url:string="http://localhost:3000";
+  url:string="http://127.0.0.1:8000/";
   constructor(private http:HttpClient) { }
 
   getPedidos() {
     return this.http.get(this.url + '/pedidos');
   }
 
-  public addProduct(product:Producto):Observable<Producto> {
-    return this.http.post<Producto>(this.url + '/products', product);
+  public agregarProducto(product:DetallePedido):Observable<any> {
+    return this.http.post(this.url + 'agregar/'+ product.idProducto, product);
   }
 
-  public updateProduct(product:Producto):Observable<Producto> {
-    return this.http.put<Producto>(this.url + '/products/' + product.id_producto, product);
-  }
+  // public updatePedido(product:Pedido):Observable<Pedido> {
+  //   return this.http.put<Pedido>(this.url + '/products/' + product.id_producto, product);
+  // }
 
-  public deleteProduct(product:Producto):Observable<Producto> {
-    return this.http.delete<Producto>(this.url + '/products/' + product.id_producto);
-  }
+  // public deleteProduct(product:Producto):Observable<Producto> {
+  //   return this.http.delete<Producto>(this.url + '/products/' + product.id_producto);
+  // }
 }
