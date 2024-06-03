2584dc11bbb97aa2a4ba2243b885e4a6549c8d4c
Agustin G <141699827+agustinnicolasgallardorios@users.noreply.github.com>
2024-06-02 21:19:20 -0300

    Actualizacion del Dashboard

--- frontend 2024/src/app/pages/dashboard/dashboard.component.ts
+++ frontend 2024/src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { StatesComponent } from './states/states.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { HistorialComponent } from './historial/historial.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [StatesComponent],
+  imports: [StatesComponent, PedidosComponent, HistorialComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 
-export class DashboardComponent {
-}
+export class DashboardComponent{}
