7a66e23e0b2c144a9f5265a57d79b075e8a5f539
cerveux <diegomartinguzman@hotmail.com.ar>
2024-06-02 21:43:41 -0300

    feat: delete cart item

--- backend 2024/Food_ISPC/appCART/urls.py
+++ backend 2024/Food_ISPC/appCART/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('agregar/<int:producto_id>/', AgregarProductoAlCarrito.as_view()),
     path('ver/', VerCarrito.as_view()),
     path('confirmar/', ConfirmarPedido.as_view()),
-    path('eliminar/<int:detalle_id>/', EliminarProductoDelCarrito.as_view()),
+    path('eliminar/<int:carrito_id>/', EliminarProductoDelCarrito.as_view()),
     path('ver_dashboard/<int:id_usuario>/', VerDashboard.as_view()),
 ]
 
