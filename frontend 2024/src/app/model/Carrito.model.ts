ff0b52a19a875d4bb48bc325b4e00f39a504ef8e
Cristian López <cristianlp.ok@gmail.com>
2024-06-03 15:26:40 -0300

    Update componente carrito

--- frontend 2024/src/app/model/Carrito.model.ts
+++ frontend 2024/src/app/model/Carrito.model.ts
@@ -1,13 +1,13 @@
 export class Carrito{
-    constructor(producto:string, idProducto:number, precio:number, cantidad:number, imageURL:string){
+    constructor(producto:string, id:number, precio:number, cantidad:number, imageURL:string){
         this.producto=producto,
-        this.id_producto=idProducto,
+        this.id=id,
         this.precio=precio,
         this.cantidad=cantidad,
         this.imageURL=imageURL
     }
     producto:string="0";
-    id_producto:number=0;
+    id:number=0;
     precio:number=0;
     cantidad:number=1;
     imageURL:string="0"
